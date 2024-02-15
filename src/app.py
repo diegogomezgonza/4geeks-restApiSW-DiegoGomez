53470f9c31562135606d3b7c2a42c678b2ec3b22
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-02-15 20:11:21 +0000

    Terminado

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -45,6 +44,137 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+# Para los endpoint que reciben toda la información, he creado rutas con el mismo nombre
+# que tiene SWAPI. Para recuperar todo el contenido de cada modelo, hago nombre.all(), una consulta
+# la cual devuelve todo el contenido que esté en cada tabla de la bd.
+# En los modelos, uso serialize para poder manipular todos los campos de las clases, de 
+# forma que a la hora de recorrer las clases lo hago llamando a serialize y después recorriendo
+# todos los campos con un for. Por último, paso a json los campos.
+
+# Todos los personajes
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    serialized_people = [people.serialize() for people in people] 
+    return jsonify(serialized_people), 200
+
+# Todos los planetas
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    serialized_planets = [planet.serialize() for planet in planets]
+    return jsonify(serialized_planets), 200
+
+# Todos los usuarios
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify(serialized_users), 200
+
+# En el caso de los favoritos, aunque sigue siendo una consulta .all, como lo que quiero
+# son todos los favoritos de un usuario concreto, hago otra consulta llamada filter_by, la cual
+# me sirve para mostrar el contenido que quiero (Favorite) filtrándolo por lo que yo indique (el id de usuario)
+
+# Los favoritos de un user concreto
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    # ID del usuario
+    user_id = 1 
+
+    # Para mostrar los favoritos de un usuario en concreto, filtro los favoritos por el id del usuario
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+
+    # Para enviar los favoritos en un formato JSON, serializo 
+    serialized_favorites = [favorite.serialize() for favorite in favorites]
+
+    return jsonify(serialized_favorites), 200
+
+# Para sacar la información de un planeta o personaje individualmente, en la ruta añado
+# el id del planeta o personaje correspondiente. En vez de hacer una consulta .all, hago una 
+# .get, que recoge la información del planeta o personaje cuyo id coincida con el que hay en la ruta.
+# Si el personaje o planeta es igual a null (no existe) aparecerá un mensaje.
+
+# La información de un personaje
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    people = People.query.get(people_id)
+    if people is None:
+        raise APIException("El personaje no existe", status_code=404)
+    return jsonify(people.serialze()), 200
+
+#La información de un planeta
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("El planeta no existe", status_code=404)
+    return jsonify(planet.serialize()), 200
+
+
+# A la hora de añadir un favorito, la ruta empieza con favorite, el tipo de favorito a almacenar y el id
+# del planeta o personaje. A la hora de añadir el favorito a la clase, añado el id del usuario actual
+# y el id del planeta o personaje según corresponda. 
+# Con session.add le indico a la bd que quiero insertar el contenido especificado una vez se haya hecho el commit
+# de la siguiente línea. Si el añadido funciona, se muestra un mensaje.
+
+# Añadir un personaje favorito al usuario
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    # Id de usuario
+    user_id = 1 
+    # Creación del favorito según del id
+    favorite = Favorite(user_id=user_id, people_id=people_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify({"message": "Personaje añadido"}), 201
+
+# Añadir un nuevo planeta favorito al usuario
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    # Id de usuario
+    user_id = 1 
+    # Creación del favorito según del id
+    favorite = Favorite(user_id=user_id, planet_id=planet_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify({"message": "Planeta añadido"}), 201
+
+# El procedimiento para borrar el favorito, tiene la misma ruta y logica a la hora de usar
+# el id del usuario, lo que cambia es el método y la consulta.
+# Con filter_by, busco el id del planeta o personaje recibido en la ruta y lo busco en la tabla
+# favorite del usuario. Con first puedo hacer una "condición" en la cual devuelve el id especificado
+# y si no se encuentra entonces devuelve None, lo cual me ayuda para la condición que hago después 
+# la cual según el valor de favorite hace el delete o no (if favorite sería cuando su valor es el id).
+
+# Borrar un planeta favorito
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    # Id de usuario
+    user_id = 1
+    # Eliminación del favorito según del id el id de usuario
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({"message": "Planeta favorito eliminado"}), 200
+    else:
+        raise APIException("El planeta no existe", status_code=404)
+
+# Borrar un personaje favorito
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    # Assume the current user is retrieved from the request context or session
+    user_id = 1 
+    # Eliminación del favorito según del id el id de usuario
+    favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({"message": "Personaje favorito eliminado"}), 200
+    else:
+        raise APIException("El personaje no existe", status_code=404)
+    
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -11,9 +11,71 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+# A la hora de hacer los modelos de People y Planets, me he basado en las propiedades que mostraba
+# en mi proyecto.
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(50))
+    hair_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    height = db.Column(db.Integer)
+    homeworld = db.Column(db.String(50))
+    birth_year = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "height": self.height,
+            "homeworld": self.homeworld,
+            "birth_year": self.birth_year,
+            "skin_color": self.skin_color
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.String(50))
+    diameter = db.Column(db.String(50))
+    gravity = db.Column(db.String(50))
+    climate = db.Column(db.String(50))
+    orbital_period = db.Column(db.String(50))
+    surface_water = db.Column(db.String(50))
+    rotation_period = db.Column(db.String(50))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            "orbital_period": self.orbital_period,
+            "surface_water": self.surface_water,
+            "rotation_period": self.rotation_period
+        }
+
+ # Las tres relaciones son de muchos a uno
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Relación con la tabla User usando la columna 'id'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # Relación con la tabla Planet usando la columna 'id'
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    # Relación con la tabla People usando la columna 'id'
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id
         }
